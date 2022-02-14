f46f268045d5a865a2fd12179f916ac10985c793
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-14 17:06:41 -0300

    login page

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,10 +1,39 @@
-import React, { useContext } from 'react';
-import AppContext from '../context/AppContext';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LOGIN_ENDPOINT = 'http://localhost:3001/login';
 
 function Login() {
-  const { state1 } = useContext(AppContext);
+  const [formData, setFormData] = useState({
+    user: '',
+  });
+
+  function handleChange({ target: { name, value } }) {
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  function postLogin() {
+    try {
+      const res = axios.post(LOGIN_ENDPOINT, formData);
+      console.log(res.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
-    <div>{state1}</div>
+    <form onSubmit={postLogin}>
+      <label htmlFor="user-input">
+        <input
+          type="text"
+          name="user"
+          id="user-input"
+          placeholder="username"
+          value={formData.user}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Entrar</button>
+    </form>
   );
 }
 
