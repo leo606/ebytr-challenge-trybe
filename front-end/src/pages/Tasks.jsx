f697738965b814fdcc49393906e7fd4c57b676a5
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-15 13:37:17 -0300

    tasks list

--- front-end/src/pages/Tasks.jsx
+++ front-end/src/pages/Tasks.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import AppContext from '../context/AppContext';
 
 function Tasks() {
+  const { user, tasks } = useContext(AppContext);
+
   return (
-    <h1>tasks</h1>
+    <>
+      <h1>tasks</h1>
+      <h1>{user.user}</h1>
+      <ul>
+        {tasks.map((t) => <li>{t.tasks}</li>)}
+      </ul>
+    </>
   );
 }
 
