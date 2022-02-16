9ee121d429ad04fa762bdbecddbebd79a6faaed3
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-16 15:39:12 -0300

    fix date

--- back-end/src/service/task/create.js
+++ back-end/src/service/task/create.js
@@ -3,7 +3,7 @@ const taskModel = require("../../model/task");
 module.exports = async (task, user) => {
   try {
     const status = "pendente";
-    const date = new Date(Date.now);
+    const date = new Date(Date.now());
     const newTask = await taskModel.insert({ task, user, status, date });
     return { id: newTask.insertedId, task, user, status, date };
   } catch (e) {
