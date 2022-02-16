88d8388243f73be1b90fbb804b71b3eb66eef576
Leonardo Ferreira <leonardoferreira@protonmail.com>
2022-02-16 08:51:05 -0300

    post login success tests

--- back-end/src/tests/login.test.js
+++ back-end/src/tests/login.test.js
@@ -8,12 +8,15 @@ const { getConnection } = require("./mocks/connectionMock");
 
 const { expect } = chai;
 
+const usersMock = require("./mocks/userMock.json");
+const tasksMock = require("./mocks/tasksMock.json");
+
 chai.use(chaiHttp);
 
 describe("post login", () => {
   let connectionMock;
   before(async () => {
-    connectionMock = await getConnection;
+    connectionMock = await getConnection();
     sinon.stub(MongoClient, "connect").resolves(connectionMock);
   });
 
@@ -37,6 +40,36 @@ describe("post login", () => {
 
     it("error message have text: user is required ", () => {
       expect(response.body).to.have.property("message", "user is required");
+    });
+  });
+
+  describe("when user is correct", () => {
+    let response;
+    before(async () => {
+      const usersCollection = await connectionMock
+        .db("ebytr-tasks")
+        .collection("users");
+      const taskCollection = await connectionMock
+        .db("ebytr-tasks")
+        .collection("tasks");
+
+      await usersCollection.insertMany(usersMock);
+      await taskCollection.insertMany(tasksMock);
+
+      response = await chai
+        .request(server)
+        .post("/login")
+        .send({ user: "abc" });
+    });
+
+    it("returns status code 200", () => {
+      expect(response).to.have.status(200);
+    });
+
+    it("returns user object", () => {
+      expect(response.body).to.have.property("user", usersMock[0].user);
+      expect(response.body).to.have.property("id", usersMock[0]._id);
+      expect(response.body).to.have.property("tasks");
     });
   });
 });
